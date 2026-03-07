2f1af376e060c6480b7f5ab87294fb52b62d5283
Sunny kumar <112869313+Sunny496167@users.noreply.github.com>
2026-03-07 07:03:34 +0530

    feat: Add email sending utility with a custom HTML template for user verification.

--- AgriBackend/src/utils/sendEmail.js
+++ AgriBackend/src/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async (name, email, confirmationCode) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
